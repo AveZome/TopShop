3f1debfaa1537821807a62aa202ddb0b7e433bfd
AveZome <alecsei.elistratov@gmail.com>
2017-03-30 01:21:18 +0300

    INDEX.PHP

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+// FRONT CONTROLLER
+
+// 1. General settings
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+session_start();
+
+// 2. Connecting system files
+define('ROOT', dirname(__FILE__));
+require_once(ROOT.'/components/Autoload.php');
+
+
+// 4. Call Router
+$router = new Router();
+$router->run();
